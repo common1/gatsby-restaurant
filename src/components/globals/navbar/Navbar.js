9c3e55e03c87db0fe11147c7456ad6616db20a07
ubuntuuser <ubuntu.user@ubuntuuser.com>
2019-02-09 15:35:10 +0100

    navbar added

---
+++ src/components/globals/navbar/Navbar.js
@@ -0,0 +1,36 @@
+import React, { Component } from "react";
+import NavbarHeader from "./NavbarHeader";
+import NavbarLinks from "./NavbarLinks";
+import NavbarIcons from "./NavbarIcons";
+import styled from "styled-components";
+
+export default class Navbar extends Component {
+  state = {
+    navbarOpen: false
+  };
+
+  handleNavbar = () => {
+    this.setState(() => {
+      return { navbarOpen: !this.state.navbarOpen };
+    });
+  };
+
+  render() {
+    return (
+      <nav>
+        <NavWrapper>
+          <NavbarHeader handleNavbar={this.handleNavbar} />
+          <NavbarLinks navbarOpen={this.state.navbarOpen} />
+          <NavbarIcons />
+        </NavWrapper>
+      </nav>
+    );
+  }
+}
+
+const NavWrapper = styled.nav`
+  @media (min-width: 768px) {
+    display: flex;
+    align-items: center;
+  }
+`;
